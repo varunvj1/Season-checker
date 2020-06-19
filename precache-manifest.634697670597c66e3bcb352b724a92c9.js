651a66fdb8e939ee86ef8ef75f23b0ca7ae1615d
Varun Jain <varunvj181@gmail.com>
2020-06-20 05:15:35 +0530

    Updates

---
+++ precache-manifest.634697670597c66e3bcb352b724a92c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b157d671257c279c9248782bc006c6",
+    "url": "/Season-checker/index.html"
+  },
+  {
+    "revision": "f52669ff1c7056d58b80",
+    "url": "/Season-checker/static/css/main.11e8ab1c.chunk.css"
+  },
+  {
+    "revision": "fa354485be05596676f5",
+    "url": "/Season-checker/static/js/2.af9bdbca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Season-checker/static/js/2.af9bdbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52669ff1c7056d58b80",
+    "url": "/Season-checker/static/js/main.e91735f8.chunk.js"
+  },
+  {
+    "revision": "fca88eb3598292f94b89",
+    "url": "/Season-checker/static/js/runtime-main.5552f139.js"
+  }
+]);
